94a32c0de5e37bb9c638d873daacd143e3f74d8e
Your Name <you@example.com>
2015-04-27 23:45:57 +0300

    ok

---
+++ app-settings.js
@@ -0,0 +1,13 @@
+(function() {
+  window.HauserAppSettings = {
+    apiBaseUrl: "http://hauser-prod.herokuapp.com/api/mobile/v1", 
+    facebookAppId: "377169545804386",
+    facebookAppName: "HauserAppProd",
+    firebaseUrl: "https://hauser.firebaseio.com",
+    imagesCdnBaseUrl: "http://hauser-prod.herokuapp.com/images",
+
+    rootUrlState: "menu.feed",
+    unauthorizedFallbackUrlState: "landing.login"
+  }
+})();
+
